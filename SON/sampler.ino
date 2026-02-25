d6f5363752bd854bbe02fc791b213b30de807ba4
Margauxval <margauxvallee09@icloud.com>
2026-02-25 09:25:56 +0100

    Enhance sampler with recording status messages
    Added serial print statements for recording status.

--- SON/sampler.ino
+++ SON/sampler.ino
@@ -2,13 +2,11 @@
 #include <Bounce.h>
 #include "sampler.h"
 
-// ================= FAUST =================
 sampler faust;
 
-// ================= AUDIO =================
-AudioInputI2S         micInput;
-AudioOutputI2S        audioOutput;
-AudioMixer4           mixer1;
+AudioInputI2S          micInput;
+AudioOutputI2S         audioOutput;
+AudioMixer4            mixer1;
 
 AudioConnection patchCord1(micInput, 0, mixer1, 0);
 AudioConnection patchCord2(mixer1, 0, faust, 0);
@@ -17,14 +15,12 @@ AudioConnection patchCord4(faust, 0, audioOutput, 1);
 
 AudioControlSGTL5000 audioShield;
 
-// ================= BOUTON =================
 const int buttonPin = 0;
 Bounce button = Bounce(buttonPin, 15);
 
-// ================= SETUP =================
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin, INPUT); // Ton bouton en Pull-down
 
   AudioMemory(140);
   audioShield.enable();
@@ -35,21 +31,23 @@ void setup() {
   mixer1.gain(0, 1.0);
 
   usbMIDI.begin();
+  Serial.println("Sampler Pret...");
 
   faust.setParamValue("note", 69.0f);
   faust.setParamValue("gate", 0.0f);
 }
 
-// ================= LOOP =================
 void loop() {
   button.update();
 
   if (button.risingEdge()) {
     faust.setParamValue("record", 1.0f);
+    Serial.println("Recording...");
   }
 
   if (button.fallingEdge()) {
     faust.setParamValue("record", 0.0f);
+    Serial.println("Stopped Recording.");
   }
 
   while (usbMIDI.read()) {
@@ -60,9 +58,11 @@ void loop() {
     if (type == usbMIDI.NoteOn && data2 > 0) {
       faust.setParamValue("note", (float)data1);
       faust.setParamValue("gate", 1.0f);
+      Serial.print("Note On: "); Serial.println(data1);
     }
     else if (type == usbMIDI.NoteOff || (type == usbMIDI.NoteOn && data2 == 0)) {
       faust.setParamValue("gate", 0.0f);
+      Serial.println("Note Off");
     }
   }
 }
